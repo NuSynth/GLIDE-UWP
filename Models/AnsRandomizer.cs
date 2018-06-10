d99cec7d6c6dfb2638212e384633fdf16fc4189f
Herbert Smith <38872463+Dartomic@users.noreply.github.com>
2018-06-10 02:39:46 -0400

    Update AnsRandomizer.cs

--- Models/AnsRandomizer.cs
+++ Models/AnsRandomizer.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,15 +9,15 @@ namespace ObservableImageTest.Models
     // This randomizes the organization of the list for the answers
     static class AnsRandomizer
     {
-        public static void Shuffle<T>(this Random rng, T[] array)
+        public static void Shuffle<T>(this Random rearrange, T[] indexArray)
         {
-            int n = array.Length;
-            while (n > 1)
+            int count = indexArray.Length;
+            while (count > 1)
             {
-                int k = rng.Next(n--);
-                T temp = array[n];
-                array[n] = array[k];
-                array[k] = temp;
+                int k = rearrange.Next(count--);
+                T temp = indexArray[count];
+                indexArray[count] = indexArray[k];
+                indexArray[k] = temp;
             }
         }
     }
