0232d8436434a0c7e2b7798e11ca9dace50b6369
Herbert Smith <38872463+Dartomic@users.noreply.github.com>
2018-06-10 04:04:56 -0400

    Update MainPage.xaml.cs

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -12,7 +12,6 @@ using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 namespace ObservableImageTest
 {
