74f050f2a859a508c417e09b8f80ceb5592b7680
Herbert Smith <38872463+Dartomic@users.noreply.github.com>
2018-06-10 02:24:22 -0400

    Add files via upload

---
+++ Models/AnsRandomizer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObservableImageTest.Models
+{
+    // This randomizes the organization of the list for the answers
+    static class AnsRandomizer
+    {
+        public static void Shuffle<T>(this Random rng, T[] array)
+        {
+            int n = array.Length;
+            while (n > 1)
+            {
+                int k = rng.Next(n--);
+                T temp = array[n];
+                array[n] = array[k];
+                array[k] = temp;
+            }
+        }
+    }
+}
