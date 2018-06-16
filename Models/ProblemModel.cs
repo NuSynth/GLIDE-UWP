68c52b5ada8e6dc8fd38db0a54f346623f8f9989
Herbert Smith <38872463+Dartomic@users.noreply.github.com>
2018-06-16 04:36:23 -0400

    Update ProblemModel.cs

--- Models/ProblemModel.cs
+++ Models/ProblemModel.cs
@@ -13,36 +13,24 @@ namespace ObservableImageTest.Models
         public int TopicID { get; set; }
         public int ProblemID { get; set; }
         public string ProblemPath { get; set; }
-        
+
         // This will be for problems (or questions) that only need characters available on the keyboard.
         public string QuestionText { get; set; }
-
     }
 
     public class ProblemManager
     {
-
         public static List<ProblemModel> GetProblems()
         {
             var Problems = new List<ProblemModel>();
 
-            Problems.Add(new ProblemModel { ProblemID = 0, ProblemPath = "Assets/male-01.png" });
-            Problems.Add(new ProblemModel { ProblemID = 1, ProblemPath = "Assets/male-02.png" });
-            Problems.Add(new ProblemModel { ProblemID = 2, ProblemPath = "Assets/male-03.png" });
-            Problems.Add(new ProblemModel { ProblemID = 3, ProblemPath = "Assets/female-01.png" });
+            // The two problems are commented out to test that answers match with problems.
+            Problems.Add(new ProblemModel { ProblemID = 0, TopicID = 0, ProblemPath = "Assets/male-01.png" });
+            Problems.Add(new ProblemModel { ProblemID = 1, TopicID = 0, ProblemPath = "Assets/male-02.png" });
+            Problems.Add(new ProblemModel { ProblemID = 2, TopicID = 1, ProblemPath = "Assets/male-03.png" });
+            Problems.Add(new ProblemModel { ProblemID = 3, TopicID = 2, ProblemPath = "Assets/female-01.png" });
 
             return Problems;
         }
-
-
-        // Not sure if I'll use this yet.
-        //public static List<Icon> GetDefault()
-        //{
-        //    var Problems = new List<ProblemModel>();
-
-        //    Problems.Add(new Problems { ProblemID = "0", IconPath = "Assets/male-01.png" });
-
-        //    return Problems;
-        //}
     }
 }
