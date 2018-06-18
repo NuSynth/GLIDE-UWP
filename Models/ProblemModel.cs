3bbe2a3b97839a34936ac21fcf7aa4340cc928a2
Herbert Smith <38872463+Dartomic@users.noreply.github.com>
2018-06-18 14:03:53 -0400

    Update ProblemModel.cs

--- Models/ProblemModel.cs
+++ Models/ProblemModel.cs
@@ -24,7 +24,6 @@ namespace ObservableImageTest.Models
         {
             var Problems = new List<ProblemModel>();
 
-            // The two problems are commented out to test that answers match with problems.
             Problems.Add(new ProblemModel { ProblemID = 0, TopicID = 0, ProblemPath = "Assets/male-01.png" });
             Problems.Add(new ProblemModel { ProblemID = 1, TopicID = 0, ProblemPath = "Assets/male-02.png" });
             Problems.Add(new ProblemModel { ProblemID = 2, TopicID = 1, ProblemPath = "Assets/male-03.png" });
